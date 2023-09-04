27ea696b3cfbde72d1a7568097e444d1b9a9d6d1
Euro Abo <abaoeuro2002@gmail.com>
2023-09-04 21:45:11 +0800

    gg

--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { HiMenuAlt3 } from "react-icons/hi";
+import "./TodoList.css";
 
 function TodoList() {
-  return <div>TodoList</div>;
+  return (
+    <div className="todolist-container">
+      <div className="header">
+        <h1>Todo List</h1>
+        <HiMenuAlt3 />
+      </div>
+    </div>
+  );
 }
 
 export default TodoList;
