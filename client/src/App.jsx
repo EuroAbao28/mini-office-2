bf160e8782005fb7ed4640fee2728bad02aa9007
Euro Abo <abaoeuro2002@gmail.com>
2023-09-06 22:34:38 +0800

    gg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,15 @@ import "./App.css";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
-import { useState } from "react";
 
 function App() {
   return (
     <div className="app-container">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </div>
